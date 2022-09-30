6b62fb5bf4cf24a7262496c3fba7e7dbe904c294
adanxx <dan_47i@hotmail.com>
2022-09-30 22:51:08 +0200

    java-testing no changes

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,15 @@
-import '../styles/styles.css'
+/*import'../styles/styles.css';
 
 if(module.hot){
     module.hot.accept();
+}*/
+
+
+function person(blank) {
+    console.log("testing "+blank);
 }
+
+var text = "world";
+
+person(text);
+
